6df8f98d4d8c2eea458788fe04f731a9143d1c32
Nielsencu <50238554+Nielsencu@users.noreply.github.com>
2021-01-07 20:43:54 +0800

    fixed the bug of database not updating when pressing callback button

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from responses import (welcome_text, menu_text, menuItem_text)
+from responses import (welcome_text, menu_text, menuItem_text, help_text)
 from telegram import ParseMode
 from keyboardmarkups import (menu_options, menuItem_options, start_options, rating_options, back_button_only)
 from models import FoodSet
@@ -19,6 +19,19 @@ def handle_home(update,context):
     return MAIN
 
 def handle_help(update,context):
+    context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = help_text(),
+        reply_markup = back_button_only(),
+        parse_mode = ParseMode.HTML
+    )
+    return MAIN
+
+def handle_seeall(update,context):
+    context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text="Western:\nMac and Cheese\nMarble baked Potato\nChicken Chop\n\nAsian:\n...\n..."
+    )
     return MAIN
 
 def handle_cumulativerating(update,context):
@@ -37,50 +50,69 @@ def handle_menu(timeofday):
 
 def handle_menu_item(menuItem):
     menu = session.query(FoodSet).filter(FoodSet.settype == menuItem).first()
-    if menu == None:
+    if menu is None:
         rating = 'Menu not found'
     else:
         try:
             rating = round(menu.rating/ menu.total_rater,2)
         except ZeroDivisionError:
-            rating = 'No ratings yet'
+            rating = 'null'
     
     def send_message(update,context):
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text= menuItem_text(menuItem = menuItem, rating = rating),
-            reply_markup = menuItem_options(menuItem = menuItem),
-            parse_mode = ParseMode.HTML
-        )
+        now = dt.now()
+        date_time = now.strftime("%Y%m%d")
+        if menu is not None:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text= menuItem_text(menuItem = menuItem, rating = rating, user_have_rated = date_time == menu.last_rated_at, number_people_rated = menu.total_rater, foodItems=['Mac and cheese', 'Marble baked Potato', 'Chicken Chop']),
+                reply_markup = menuItem_options(menuItem = menuItem),
+                parse_mode = ParseMode.HTML
+            )
+        else:
+            context.bot.send_message(
+                chat_id = update.effective_chat.id,
+                text="Not found"
+                )
         return MAIN
     return send_message
 
-def handle_rating_number(update,context):
-    context.bot.send_message(
-        chat_id = update.effective_chat.id,
-        text = "Please rate from 1-5",
-        reply_markup = rating_options(),
-        )
-    return MAIN
+def handle_rating_number(menuItem):
+
+    def ask_to_rate(update,context):
+        context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = "Please rate from 1-5",
+            reply_markup = rating_options(menuItem),
+            )
+        return MAIN
+    return ask_to_rate
 
 def handle_rating(menuItem,rating):
     
     def update_rating(update,context):
-        menu = session.query(FoodSet).filter(FoodSet.settype == menuItem).first()
 
         session.query(FoodSet).filter(FoodSet.settype == menuItem).update({"rating": FoodSet.rating + rating, "total_rater": FoodSet.total_rater + 1})
-        '''
-        now = dt.now()
-        date_time = now.strftime("%Y%m%d")
-        if date_time == menu.last_rated_at:
-            context.bot.send_message(chat_id= update.effective_chat.id, text="You have rated already!")
-        else:
-            session.query(FoodSet).filter(FoodSet.settype == menuItem).update({"rating": FoodSet.rating + rating, "last_rated_at": date_time, "total_rater": FoodSet.total_rater + 1})
-        '''
-            
         session.commit()
-        return MAIN
-
+        return handle_menu_item(menuItem=menuItem)(update,context)
+        """
+        now = dt.now()  # Rate only once a day
+        date_time = now.strftime("%Y%m%d")
+        if not(date_time == menu.last_rated_at):
+            session.query(FoodSet).filter(FoodSet.settype == menuItem).update({"rating": FoodSet.rating + rating, "last_rated_at": date_time, "total_rater": FoodSet.total_rater + 1})
+            session.commit()
+        else:
+            return handle_rated_already"""
+    
     return update_rating
+
+def handle_rated_already(update,context):
+
+    context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = "You have rated already!"
+    )
+
+    return MAIN
+
 
 
--- responses.py
+++ responses.py
@@ -5,8 +5,21 @@ def welcome_text(user_firstname):
 def menu_text():
     return f'Click to see items and rate!'
 
-def menuItem_text(menuItem, rating):
-    return f'{menuItem} Menu Items\n\n' + f"Today's rating for {menuItem} is {rating}"
+def menuItem_text(menuItem, foodItems, rating, user_have_rated, number_people_rated):
+    addedText=''
+    for item in foodItems:
+        addedText += item + '\n'
+    if user_have_rated:
+        addedText += '\nYou have rated this menu!'
+    else:
+        addedText += '\nYou have not rated this menu yet!'
+    if rating == 'null':
+        return addedText + f"\n\nCurrent rating: No ratings yet!" 
+    else:
+        return f'<b>{menuItem} Menu Items:</b>\n\n' +  addedText + f"\n\nCurrent rating: ★<b> {rating}</b> ({number_people_rated})"
 
 def cumulativeRating_text():
     return f"Here are the cumulative ratings of the menus this week"
+
+def help_text():
+    return f'What can this bot do?\n' + '• Allows you to view breakfast and dinner menu!\n' + '• Do attach a picture of the menu items if no one has done so!\n' + '• Allows you to rate menu items by category!\n' + '• Breakfast winner and dinner winner announced daily and weekly?\n'
